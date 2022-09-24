215e90c1f7da43d188a72231feb823257ec0947d
nimish shinde <shinde.nimish3001@gmail.com>
2022-09-24 12:33:09 +0530

    himalaya login update

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+import MainRoutes from './Routes/MainRoutes';
+
+function App() {
+  return (
+    <div className="App">
+     {/* <Home/> */}
+     {/* <Navbar/> */}
+     {/* <Register/> */}
+     <MainRoutes/>
+     {/* <OTPPAGE/> */}
+     {/* <Otp/> */}
+     {/* <Insta/> */}
+     {/* <Searchbar/> */}
+     {/* <Toggle/> */}
+     {/* <Success/> */}
+
+    </div>
+  );
+}
+
+export default App;
